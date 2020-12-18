93bb16af69715c0a3d26b8dd093ab010a3499436
Tom <tomnijbroek@gmail.com>
2020-12-18 10:18:54 +0100

    Homework solver

--- Advent_Code_2020/Day18/homework_solver_pt2.py
+++ Advent_Code_2020/Day18/homework_solver_pt2.py
@@ -9,7 +9,21 @@ test_input = ['2 * (3 + (4 * 5) + 6)'] # = 2* (3 + 20 + 6) = 58
 def int_product(a,b):
     return a*b
 
-def solve_no_par(exersize): #exersize is a list of integers and operators
+def solve_no_par_plus_prio(exersize,pt_1): #exersize is a list of integers and operators
+    exersize = exersize.split(' ')
+    #work through + operators first
+    if not pt_1:
+        while exersize.count('+')>0:
+            plus_ind = [ind for ind, char in enumerate(exersize) if char == '+']
+            plus = plus_ind[0]
+            addition = int(exersize[plus-1]) + int(exersize[plus+1])
+
+            exersize.insert(plus - 1,str(addition))
+            exersize.pop(plus)
+            exersize.pop(plus)
+            exersize.pop(plus)
+
+
     ans = int(exersize[0]) #start with first number
     for ind in range(1,len(exersize)):
         element = exersize[ind]
@@ -22,15 +36,17 @@ operator_dict = {'+': add,
                  '*': int_product}
 
 tot_ans = 0
-for exersize in input:
-    while exersize.count('+') > 0:
-        left_addition = [ind-2 for ind,char in enumerate(exersize) if char == '+']
-        right_addition = [ind+2 for ind,char in enumerate(exersize) if char == '+']
-        additions = [exersize[left:right for left,right in zip(left_addition,right_addition) ]
-        sub_ex = exersize[right_par_open+1:right_par_close]
-        sub_ans = solve_no_par(sub_ex.split(' '))
-        exersize = exersize[0:right_par_open]+str(sub_ans)+ exersize[right_par_close+1:] asdf
+for pt_1 in (True,False):
+    for exersize in input:
+        while exersize.count('(') > 0:
+            par_open_ind = [ind for ind,char in enumerate(exersize) if char == '(']
+            right_par_open = max(par_open_ind)
+            par_close_ind = [ind for ind,char in enumerate(exersize) if char == ')' and ind > right_par_open]
+            right_par_close = par_close_ind[0]
+            sub_ex = exersize[right_par_open+1:right_par_close]
+            sub_ans = solve_no_par_plus_prio(sub_ex,pt_1)
+            exersize = exersize[0:right_par_open]+str(sub_ans)+ exersize[right_par_close+1:]
 
-    exersize = exersize.split(' ')
-    tot_ans += solve_no_par(exersize)
-print(tot_ans)
+        tot_ans += solve_no_par_plus_prio(exersize,pt_1)
+    print(pt_1)
+    print(tot_ans)
