571712018f1cd87806d034d8993093fb143c913f
TomNij <tom@tacx.com>
2021-11-18 16:11:53 +0100

    2019 start

--- Advent_Code_2015/Day21/rpg_simulator.py
+++ Advent_Code_2015/Day21/rpg_simulator.py
@@ -1,7 +1,7 @@
 from itertools import combinations
 
 shop = {
-    'weapons':{
+    'Weapons':{
     'Dagger':        [8,4,0],
     'Shortsword':   [10,5,0],
     'Warhammer'  :  [25,     6,       0],
@@ -18,6 +18,7 @@ shop = {
             },
     'Rings': {
     'Null'     : [0,     0,       0],
+    'Null2'     : [0,     0,       0],
     'Damage +1':   [25,     1,       0],
     'Damage +2' :  [50,      2,       0],
     'Damage +3'  : [100,     3,       0],
@@ -26,12 +27,53 @@ shop = {
     'Defense +3'  : [80,     0,       3]}
 }
 
-boss = {'Health' : 103,
+boss = {'Health': 103,
 'Damage': 9,
 'Armor': 2}
 
-#requirements: 1 weapon, 0-1 armor, 0-2 rings
-#try bossfight with increased spending
+player = {'Health': 100,
+'Damage': 0,
+'Armor': 0}
+
+# requirements: 1 weapon, 0-1 armor, 0-2 rings
+# try bossfight with increased spending
 # for weapon in weapons
 # for armor in armor
 # for ring in rings + combination(rings,2)
+
+def battle_win(boss,player):
+    while True:
+        #start with player attacks boss
+        boss['Health'] -= max(1,player['Damage']-boss['Armor'])
+        if boss['Health'] <= 0:
+            return True #player wins
+        # boss attacks player
+        player['Health'] -= max(1, boss['Damage'] - player['Armor'])
+        if player['Health'] <= 0:
+            return False  # boss wins
+
+min_cost = 99999999
+max_cost = 0
+for weapon in shop['Weapons']:
+    weapon_price,weapon_dmg,_ = shop['Weapons'][weapon]
+    for armor in shop['Armor']:
+        armor_price,_, armor_armor = shop['Armor'][armor]
+        for ring_combo in combinations(shop['Rings'],2):
+            ring_price = sum([shop['Rings'][ring][0] for ring in ring_combo])
+            ring_dmg = sum([shop['Rings'][ring][1] for ring in ring_combo])
+            ring_armor = sum([shop['Rings'][ring][2] for ring in ring_combo])
+            player['Damage'] = weapon_dmg + ring_dmg
+            player['Armor'] = armor_armor + ring_armor
+            total_cost = weapon_price + armor_price + ring_price
+            player_battle = dict(player)
+            boss_battle = dict(boss)
+            if battle_win(boss_battle, player_battle):
+                if total_cost < min_cost:
+                    min_cost = total_cost
+            else:
+                if total_cost > max_cost:
+                    max_cost = total_cost
+
+
+print(f"Part 1: {min_cost}")
+print(f"Part 2: {max_cost}")
