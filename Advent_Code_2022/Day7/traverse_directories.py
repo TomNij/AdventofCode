fcf358bf6655a8ee1e9ae34753a98f45300d7b2f
TomNij <tom@tacx.com>
2022-12-12 15:58:49 +0100

    2022 day 7 from hell

--- Advent_Code_2022/Day7/traverse_directories.py
+++ Advent_Code_2022/Day7/traverse_directories.py
@@ -1,11 +1,11 @@
 
-with open('../Input_files/7_test.txt') as file:
+with open('../Input_files/7.txt') as file:
     input = [line.strip() for line in file.readlines()]
 
 class Dir:
     def __init__(self,name,parent):
         self.parent = parent
-        self.name = name
+        self.name = parent.name + '/' + name if parent != 'NA' else name
         self.files = {}
         self.subdir = {}
 
@@ -26,30 +26,10 @@ class Dir:
 
     def find_parent(self):
         return self.parent
-        # if root.subdir == []:
-        #     return None
-        #
-        # if name in root.subdir:
-        #     return root
-        #
-        #
-        # # If target is present in tree, then prints the parent node
-        # #fix for true recorsion!!!
-        # for dir in root.subdir:
-        #     p = find_parent(name, dir)
-        #     if p is None:
-        #         for dir in dir.subdir:
-        #             p = find_parent(name, dir)
-        #         if p is not None:
-        #             return p
-        #     else:
-        #         return p
 
 
+tot_sum = 0
 for count,cmd in enumerate(input):
-    #match cmd:
-    #    case '$ cd /':
-    #only happens on start of file
     if cmd == '$ cd /':
         root = Dir('/','NA')
         curr_dir = root
@@ -61,6 +41,7 @@ for count,cmd in enumerate(input):
     elif cmd.split(' ')[0].isnumeric():
         size,name = cmd.split(' ')
         curr_dir.addfiles(name,int(size))
+        tot_sum += int(size)
     elif cmd.split(' ')[1] == 'cd':
         #change directory
         if cmd.split(' ')[2] == '..':
@@ -70,27 +51,15 @@ for count,cmd in enumerate(input):
             new_dir = cmd.split(' ')[2]
             curr_dir = curr_dir.getsubdir(new_dir)
 
-dir_sizes = []
-def traverse_dir(root,dir_sizes):
-    #implement breat first search
-    if len(root.files.keys()) > 0:
-        dir_size = sum(root.files.values())
-        if len(root.subdir.keys()) > 0:
-            sub_list = []
-            for name, subdir in root.subdir.items():
-                traverse_dir(subdir, sub_list)
-            dir_size += sum(sub_list)
-        dir_sizes.append(dir_size)
-    if len(root.subdir.keys()) > 0:
-        for name,subdir in root.subdir.items():
-            traverse_dir(subdir,dir_sizes)
 
 def getTotalFileSize(dir):
+    size_dict = {}
     file_sum = 0
     if len(dir.subdir.keys()) == 0:
         if len(dir.files.values()) > 0:
             file_sum += sum(dir.files.values())
         #base case, no subdirectories
+        global_dict[dir.name] = file_sum
         size_dict[dir.name] = file_sum
         return size_dict
     else:
@@ -98,19 +67,28 @@ def getTotalFileSize(dir):
         if len(dir.files.values()) > 0:
             file_sum += sum(dir.files.values())
         #go into each subdirectory and get the dict of dir and file sizes
-        subdir_list = list(dir.subdir.values())
+        subdir_list = list(dir.subdir.values())[::-1]
+        sub_dir_sum = 0
         for subdir in subdir_list:
             subdir_dict = getTotalFileSize(subdir)
+            sub_dir_sum += sum(subdir_dict.values())
             #size_dict.update(subdir_dict)
-            file_sum += sum(subdir_dict.values())
-        size_dict[dir.name] = file_sum
+        size_dict[dir.name] = file_sum + sub_dir_sum
+        global_dict[dir.name] = file_sum + sub_dir_sum
+        return size_dict
 
 
-
-size_dict = {}
-dir_output = getTotalFileSize(root)
+global_dict ={}
+getTotalFileSize(root)
 # 1.762.286 too high, 1.253.145 too low
-print('Part 1:',sum([dir_size for dir_size in dir_sizes if dir_size <= 100000]))
+print('Part 1:',sum([dir_size for dir_size in global_dict.values() if dir_size <= 100000]))
+
+tot_size = 70000000
+file_size = max(global_dict.values())
+update_size = 30000000
+to_remove = update_size - (tot_size - file_size)
+#print min dir size that is bigger than the size we need to remove
+print('Part 2:',min([dir_size for dir_size in global_dict.values() if dir_size >= to_remove]))
 
 
 
